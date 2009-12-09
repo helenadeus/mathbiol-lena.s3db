858cd6383f3da2ca74f473fa7f531e78686c1a08
helenadeus <helenadeus@localhost>
2009-12-09 16:16:04 -0600

    fixed translator

--- s3qltranslator.js
+++ s3qltranslator.js
@@ -1,3 +1,8 @@
+
+String.prototype.trim = function () {
+    return this.replace(/^\s*/, "").replace(/\s*$/, "");
+}
+
 var S3QLtranslator = function (query) {
 	//start of by reading all that is before the first |
 	var entityNames = {"S":"statement", "R": "rule","C":"collection","I":"item","P":"project","U":"user","D":"deployment"};
@@ -7,25 +12,27 @@ var S3QLtranslator = function (query) {
 	if(op) { 
 	op = op[0].trim(); 
 	var targetAndParams = query.replace(op,"").trim().match(/\((.*)\)/);
-	 if(!targetAndParams){
+	
+	if(!targetAndParams){
 		alert('invalid query - parameters are required to be inside parenthesis');
 		return false;
 		}
+	targetAndParams = targetAndParams[1];
 	}
 	else {
 		op = "select";
-		var targetAndParams = query;
+		var targetAndParams = query.trim();
 	}
 	
 	
-	var target = targetAndParams[1].trim().match(/^(D|P|C|R|I|S)/);
+	var target = targetAndParams.trim().match(/^(D|P|C|R|I|S)/);
 	if(!target){
 		alert('invalid query - one of D|P|C|R|I|S is required to initialize the query');
 		return false;
 	}
 	target = target[1];
 	
-	var params = targetAndParams[1].trim().match(/\|(.*)/)[1].trim();
+	var params = targetAndParams.trim().match(/\|(.*)/)[1].trim();
 
 	//Detect if there is more than 1 paramenter
 	var s3ql_params = "";
@@ -52,10 +59,14 @@ var S3QLtranslator = function (query) {
 		s3ql_query += "<S3QL><select>*</select><from>"+entityNames[target]+"</from>";
 	}
 	else {
-		s3ql_query += "<S3QL><"+op+"><from>"+entityNames[target]+"</from>";
+		s3ql_query += "<S3QL><"+op+">"+entityNames[target]+"</"+op+">";
 	}
 	s3ql_query += s3ql_params;
 	s3ql_query += "</S3QL>";
+	var span = document.createElement('span');
+	document.body.appendChild(span);
+	span.style.fontWeight = "bold";
+	span.innerHTML = "Your query {"+query+"} translates into XML S3QL format:";
 	var txt = document.createElement('textarea');
 	document.body.appendChild(txt);
 	txt.cols = 100; 
@@ -71,7 +82,3 @@ else {
 	alert("Please provide a query in the URL using ? (for example ?select(S | R = x))");
 }
 
-
-String.prototype.trim = function () {
-    return this.replace(/^\s*/, "").replace(/\s*$/, "");
-}
